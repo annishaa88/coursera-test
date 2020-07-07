4a3914cb9e0989ccdb185a9e89e64a7933ee5deb
anna itzhaki <anna.itzhaki@verizonmedia.com>
2020-07-07 12:00:01 +0300

    assignment 1

---
+++ assignments/assignment1/assignment1-starter-code/app.js
@@ -0,0 +1,26 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp', [])
+.controller('MenuController', MenuController);
+
+MenuController.$inject = ['$scope'];
+function MenuController($scope) {
+  $scope.msg = "msg";
+  $scope.items = "";
+
+  $scope.checkIfTooMuch = function () {
+    var splitItems = $scope.items.split(',')
+    console.log(splitItems)
+    if ($scope.items == ""){
+      $scope.msg = "Please enter data first"
+    } else if (splitItems.length <= 3) {
+      $scope.msg = "Enjoy!"
+    } else {
+      $scope.msg = "Too much!"
+    }
+
+  };
+}
+
+})();
