e208d2c2860462b56f2f270adbcde3efab3fd142
anna itzhaki <anna.itzhaki@verizonmedia.com>
2020-07-20 14:48:17 +0300

    assignment 4

---
+++ assignments/assignment4/src/routes.js
@@ -0,0 +1,51 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // *** Set up UI states ***
+  $stateProvider
+
+  // Home page
+  .state('home', {
+    url: '/',
+    templateUrl: 'src/menuapp/templates/home.template.html'
+  })
+
+  // categories
+  .state('categories', {
+    url: '/categories',
+    templateUrl: 'src/menuapp/templates/categories.template.html',
+    controller: 'CategoriesController as categoriesCtrl',
+    resolve: {
+      categories: ['MenuDataService', function (MenuDataService) {
+        return MenuDataService.getAllCategories();
+      }]
+    }
+  })
+
+  // Item detail
+  .state('items', {
+    url: '/items/{categoryShortName}',
+    templateUrl: 'src/menuapp/templates/items.template.html',
+    controller: 'ItemsController as itemsCtrl',
+    params: {
+      categoryShortName: null,
+    },
+    resolve: {
+      items: ['MenuDataService', '$stateParams', function (MenuDataService, $stateParams) {
+        return MenuDataService.getItemsForCategory($stateParams.categoryShortName);
+      }]
+    }
+  });
+
+}
+
+})();
